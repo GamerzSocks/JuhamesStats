2fa2c73d643ca9fc6863fafc9031aed59d03f5f4
Gamerz <136964406+GamerzSocks@users.noreply.github.com>
2023-11-21 18:16:49 +0800

    initial
    woo, first thingy!

---
+++ JahamesStatTracker.py
@@ -0,0 +1,20 @@
+# donaghey stat checker
+print("----------------------------------")
+print("")
+print("")
+print("       JAMES STAT TRACKER")
+print("")
+print("")
+print("----------------------------------")
+print("")
+print("Made by Luke Bradley, 2023.")
+print("Statistics provided by PlayCricket.")
+print("")
+print("")
+# VARIABLES SETUP
+year = input("What grade contains the stats that you want? (e.g: U13 = Under 13's) ")
+round_number = input("What round was it? ")
+
+if year == "U13":
+    if round_number == "6":
+        print("GAME WAS NOT PLAYED BY JAMES.")
