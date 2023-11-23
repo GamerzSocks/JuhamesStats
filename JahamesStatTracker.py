f1caaa941dc14a99e670e2e109bbb469c4fdb061
Gamerz <136964406+GamerzSocks@users.noreply.github.com>
2023-11-23 16:26:15 +0800

    update

--- JahamesStatTracker.py
+++ JahamesStatTracker.py
@@ -1,4 +1,4 @@
-# donaghey stat checker
+
 print("----------------------------------")
 print("")
 print("")
@@ -17,4 +17,135 @@ round_number = input("What round was it? ")
 
 if year == "U13":
     if round_number == "6":
+        print("----------------------------------")
+        print("")
+        print("")
         print("GAME WAS NOT PLAYED BY JAMES.")
+        print("")
+        print("")
+        print("----------------------------------")
+
+# BOOKMARK, 21/11/23
+
+if year == "U13":
+    if round_number == "5":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("   BATTING: 9 of 45 balls.")
+        print( "  Run Out. (T Doyle, E Karanikis)"  )
+        print("")
+        print("   BOWLING: 4-20 of 3 Overs.")
+        print("")
+        print("   FIELDING: C-RO-ST ")
+        print("             0-0-0")
+        print("")
+        print("   RESULT: MBJCC: 7-161 - BJCC: 117")
+        print("    MBJCC win by 44 runs.")
+        print("")
+        print("----------------------------------")
+
+
+if year == "U13":
+    if round_number == "4":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("   BATTING: 0 of 1 ball.")
+        print( "  c: O Scriven b: T McPherson"  )
+        print("")
+        print("   BOWLING: 0-17 of 3 Overs.")
+        print("")
+        print("   FIELDING: C-RO-ST ")
+        print("             0-0-0")
+        print("")
+        print("   RESULT: MBJCC: 75 - SJCC COMETS: 7-182")
+        print("    Match surrendered to MBJCC. Reason: Cheating")
+        print("")
+        print("----------------------------------")
+
+if year == "U13":
+    if round_number == "3":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("   BATTING: 0 of 2 balls.")
+        print( "  b: V Kannan)"  )
+        print("")
+        print("   BOWLING: 1-38 of 4 Overs.")
+        print("")
+        print("   FIELDING: C-RO-ST ")
+        print("             0-0-0")
+        print("")
+        print("   RESULT: MBJCC: 64 - BLJCC: 149")
+        print("    BLJCC wins by 85 runs.")
+        print("")
+        print("----------------------------------")
+
+if year == "U13":
+    if round_number == "2":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("   BATTING: 3 of 34 balls.")
+        print( "  b: J Xanthis"  )
+        print("")
+        print("   BOWLING: 1-29 of 3 Overs.")
+        print("")
+        print("   FIELDING: C-RO-ST ")
+        print("             0-0-0")
+        print("")
+        print("   RESULT: MBJCC: 81 - SMCC Maroon: 8-94")
+        print("   SMCC Maroon win by 13 runs.")
+        print("")
+        print("----------------------------------")
+
+if year == "U13":
+    if round_number == "1":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("   BATTING: 2 of 19 balls.")
+        print( "  run out (S McGorm)"  )
+        print("")
+        print("   BOWLING: 2-21 of 3 Overs.")
+        print("")
+        print("   FIELDING: C-RO-ST ")
+        print("             0-0-0")
+        print("")
+        print("   RESULT: MBJCC: 91 - SMCC Blue: 5-130")
+        print("    SMCC Blue win by 39 runs.")
+        print("")
+        print("----------------------------------")
+
+# bookmark: 22/11/2023
+
+if year == "U12":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("DATA NOT YET PROVIDED.")
+        print("")
+        print("")
+        print("----------------------------------")
+
+
+if year == "U11":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("DATA NOT YET PROVIDED.")
+        print("")
+        print("")
+        print("----------------------------------")
+
+if year == "U10":
+        print("----------------------------------")
+        print("")
+        print("")
+        print("DATA NOT YET PROVIDED.")
+        print("")
+        print("")
+        print("----------------------------------")
+
+# Bookmark 23/11/2023
